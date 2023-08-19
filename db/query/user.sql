2d774e9e6f498e5a06bc0265373601ede922965a
Yaroslav Fedyna <fedynaslava@gmail.com>
2023-08-19 15:09:55 +0300

    sqlc init

---
+++ db/query/user.sql
@@ -0,0 +1,9 @@
+-- name: CreateUser :one
+INSERT INTO users (
+    username,
+    hashed_password
+) VALUES ($1, $2) RETURNING *;
+
+-- name: GetUser :one
+SELECT * FROM users
+WHERE username = $1 LIMIT 1;
